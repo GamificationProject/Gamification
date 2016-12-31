bee9cc62edf9d4a66f5fc2f7382636a8aa0ea851
Jatin Shukla <jatinshukla.lek@gmail.com>
2016-12-31 21:07:09 +0530

    User Registration Done

--- webportal/gamification/views.py
+++ webportal/gamification/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Game
+from .forms import UserForm
+from django.views.generic import View
+from django.contrib.auth import authenticate, login
 
 class IndexView(generic.ListView):
     template_name = 'gamification/index.html'
@@ -9,8 +12,34 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Game.objects.all()
     
-class RegisterView():
-    pass
+class RegisterView(View):
+    form_class = UserForm
+    template_name = 'gamification/registration_form.html'
+    
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit = False)
+            username = form.cleand_data['username']
+            first_name = form.cleand_data['first_name']
+            last_name = form.cleand_data['last_name']
+            password = form.cleand_data['password']
+            user.set_password(password)
+            user.save()
+            
+            user = authenticate(username = username, password = password)
+            
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    return redirect('gamification:index')
+        
+        return render(request, self.template_name, {'form': form})        
 
-class LoginView():
+class LoginView(generic.FormView):
     pass
